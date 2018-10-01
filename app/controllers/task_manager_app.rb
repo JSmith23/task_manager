f768bac95ed0498a792e18f7f70e94df6fcd23c7
Jimmy Smith <jaystansmith@gmail.com>
2018-09-30 21:44:57 -0600

    Completed Code

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -8,7 +8,7 @@ class TaskManagerApp < Sinatra::Base
     end
 
     get '/tasks' do
-        @tasks = ["task1", "task2", "task3"]
+        @tasks = Task.all
         erb :index
       end
 
@@ -19,6 +19,27 @@ class TaskManagerApp < Sinatra::Base
       post '/tasks' do
         task = Task.new(params[:task])
         task.save
+        redirect '/tasks'
+      end
+
+      get '/tasks/:id' do
+        @task = Task.find(params[:id])
+        erb :show
+      end
+
+      get '/tasks/:id/edit' do
+        @task = Task.find(params[:id])
+        erb :edit
+      end
+
+      set :method_override, true  # this allows us to use _method in the form
+      put '/tasks/:id' do |id|
+        Task.update(id.to_i, params[:task])
+        redirect "/tasks/#{id}"
+      end
+
+      delete '/tasks/:id' do |id|
+        Task.destroy(id.to_i)
         redirect '/tasks'
       end
   end
